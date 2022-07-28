6c1e1cde60261431a6f55fb9a92a568d1b3a4b98
fastdaima <sivaramakrishnang25@gmail.com>
2022-07-28 15:43:00 +0530

    working ex

--- testing_pysnow.py
+++ testing_pysnow.py
@@ -1,65 +1,51 @@
 from pysnow import Client, QueryBuilder
-from pprint import pprint
+import argparse
 
-client = Client("instance_name", user="username", password="password") # instance_name without
-# service-now.com
-incident = client.resource(api_path='/table/incident')
+ap = argparse.ArgumentParser()
 
-qb = (QueryBuilder().field('priority').less_than_or_equal(3).AND().field('state').equals("3"))
-
-response = incident.get(
-    query=qb,
-)
-
-ticket_numbers = []
-for record in response.all():
-    #ticket_numbers.append((record['number'], record['impact'], record['urgency'], record['comments_and_work_notes']))
-    number, impact, urgency, comments_work_notes = record['number'], int(record['impact']), int(record['urgency']), record['comments_and_work_notes']
-
-    if impact == urgency:
-        impact += 1
-    elif impact < urgency:
-        impact += 1
-    elif urgency < impact:
-        urgency += 1
-
-    update = {
-        'impact': str(impact),
-        'urgency': str(urgency),
-        'comments_and_work_notes': comments_work_notes + "Priority reduced by 1"
-    }
-
-    updated_record = incident.update(query={'number': number}, payload=update)
-    print(updated_record['priority'], updated_record['comments_and_work_notes'], updated_record['number'])
-    break
+ap.add_argument('-i', '--instance_name', required=True, help='Instance name of service now')
+ap.add_argument('-u', '--username', required=True, help='username of the service now instance')
+ap.add_argument('-p', '--password', required=True, help='password of the service now instance')
 
 
-#print(ticket_numbers)
-# Impact	Urgency	Priority
-# 1 - High	1 - High	1 - Critical
-# 1 - High	2 - Medium	2 - High
-# 1 - High	3 - Low	3 - Moderate
-# 2 - Medium	1 - High	2 - High
-# 2 - Medium	2 - Medium	3 - Moderate
-# 3 - Low	1 - High	3 - Moderate
+def update_priority(instance_name, username, password):
+    client = Client(instance_name, user=username, password=password)  # instance_name without
+    # service-now.com
+    incident = client.resource(api_path='/table/incident')
 
-# 2 - Medium	3 - Low	4 - Low
-# 3 - Low	2 - Medium	4 - Low
-# 3 - Low	3 - Low	5 - Planning
-# for ticket_number, priority_val, comment in ticket_numbers:
-#     print(ticket_number, priority_val)
-#     update = {
-#         'priority': str(priority_val+1),
-#         # change impact and urgency
-#         'description': comment + "Reduced priority by 1"
-#     }
-#     updated_record = incident.update(query={'number': ticket_number}, payload=update)
-#     pprint(updated_record)
-#     print(updated_record['priority'], updated_record['description'], updated_record['number'])
-#     break
+    qb = (QueryBuilder().field('priority').less_than_or_equal(3).AND().field('state').equals("3"))
 
-# [('INC0000007', 1), ('INC0000017', 1), ('INC0000002', 1), ('INC0000054', 1), ('INC0000040', 3)]
-# [('INC0000040', 3, ''), ('INC0000007', 1, ''), ('INC0000017', 1, ''), ('INC0000002', 1, ''), ('INC0000054', 1, '')]
-# INC0000040 3
-# <Response [200 - PUT]>
-# 3 Reduced priority by 1 INC0000040
+    response = incident.get(
+        query=qb,
+    )
+
+    output = []
+    for record in response.all():
+        number = record['number']
+        impact = int(record['impact'])
+        urgency = int(record['urgency'])
+        comments_work_notes = record['comments_and_work_notes']
+
+        if impact == urgency:
+            impact += 1
+        elif impact < urgency:
+            impact += 1
+        elif urgency < impact:
+            urgency += 1
+
+        update = {
+            'impact': str(impact),
+            'urgency': str(urgency),
+            'comments_and_work_notes': comments_work_notes + "Priority reduced by 1"
+        }
+
+        response = incident.update(query={'number': number}, payload=update)
+        output.append(
+            f"Updated: {response['number']}, new priority: {response['priority']}, response: {response['comments_and_work_notes']}")
+
+    print(output)
+
+
+if __name__ == '__main__':
+    args = vars(ap.parse_args())
+    update_priority(args['instance_name'], args['username'], args['password'])
